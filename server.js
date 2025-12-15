d5fb74fe529e365741b413881faaad11b0a234d3
nguyenbach0423 <nguyenbach0423@gmail.com>
2025-12-15 22:29:54 +0700

    new features

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 
 app.get("/", (_, res) => res.sendFile(path.join(__dirname, "index-01.html")));
-app.get("/about", (_, res) => res.sendFile(path.join(__dirname, "index-02.html")));
+app.get("/nhagai", (_, res) => res.sendFile(path.join(__dirname, "index-02.html")));
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
