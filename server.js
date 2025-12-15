15dc4177863d5498a9618d2eec378d372130e900
Bach Nguyen <bachnv@teca.vn>
2025-12-15 20:06:02 +0700

    bew features

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get("/", (_, res) => res.sendFile(path.join(__dirname, "index-01.html")));
+app.get("/about", (_, res) => res.sendFile(path.join(__dirname, "index-02.html")));
+
+app.listen(PORT);
